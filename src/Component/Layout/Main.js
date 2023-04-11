11c3eebe02b22a3fd2514b08c07c4554e444de90
mr7aali <mr7aali@gmail.com>
2023-04-11 21:35:28 +0600

    hero i con

--- src/Component/Layout/Main.js
+++ src/Component/Layout/Main.js
@@ -6,9 +6,9 @@ import { Outlet } from 'react-router-dom';
 const Main = () => {
     return (
         <div>
-            <Header />
+            {/* <Header /> */}
                <Outlet />
-            <Footer />
+            {/* <Footer /> */}
         </div>
     );
 };
