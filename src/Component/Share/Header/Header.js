47dc7f5acf3ed5936b457ed00d35c88306d334f1
mr7aali <mr7aali@gmail.com>
2023-04-12 17:09:50 +0600

    fixed err

--- src/Component/Share/Header/Header.js
+++ src/Component/Share/Header/Header.js
@@ -14,48 +14,44 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
+
 const drawerWidth = 240;
 const navItems = ['Home', 'About', 'Contact'];
 
+export default function Header(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
 
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
 
-const Header = (props) => {
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        MUI
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
 
-    const { window } = props;
-    const [mobileOpen, setMobileOpen] = React.useState(false);
+  const container = window !== undefined ? () => window().document.body : undefined;
 
-    const handleDrawerToggle = () => {
-        setMobileOpen((prevState) => !prevState);
-      };
-
-
-      const drawer = (
-        <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
-          <Typography variant="h6" sx={{ my: 2 }}>
-            MUI
-          </Typography>
-          <Divider />
-          <List>
-            {navItems.map((item) => (
-              <ListItem key={item} disablePadding>
-                <ListItemButton sx={{ textAlign: 'center' }}>
-                  <ListItemText primary={item} />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-        </Box>
-      );
-      const container = window !== undefined ? () => window().document.body : undefined;
-
-    return (
-
-        <>
-
-          <Box sx={{ display: 'flex' }}>
+  return (
+    <Box sx={{ display: 'flex', }}>
       <CssBaseline />
-      <AppBar component="nav">
-        <Toolbar>
+      <AppBar component="nav" >
+        <Toolbar className='container mx-auto'>
           <IconButton
             color="inherit"
             aria-label="open drawer"
@@ -73,15 +69,27 @@ const Header = (props) => {
             MUI
           </Typography>
           <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-            {navItems.map((item) => (
-              <Button key={item} sx={{ color: '#fff' }}>
-                {item}
-              </Button>
-            ))}
+            
+              <Link to='/'>
+                <Button  sx={{ color: '#fff' }}>
+                  Home
+                </Button>
+              </Link>
+              <Link to='/'>
+                <Button  sx={{ color: '#fff' }}>
+                  Login
+                </Button>
+              </Link>
+              <Link to='/'>
+                <Button  sx={{ color: '#fff' }}>
+                  Log Out
+                </Button>
+              </Link>
+            
           </Box>
         </Toolbar>
       </AppBar>
-      <Box component="nav">
+      <Box component="nav" >
         <Drawer
           container={container}
           variant="temporary"
@@ -98,46 +106,16 @@ const Header = (props) => {
           {drawer}
         </Drawer>
       </Box>
-      <Box component="main" sx={{ p: 3 }}>
-        <Toolbar />
-        <Typography>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique unde
-          fugit veniam eius, perspiciatis sunt? Corporis qui ducimus quibusdam,
-          aliquam dolore excepturi quae. Distinctio enim at eligendi perferendis in
-          cum quibusdam sed quae, accusantium et aperiam? Quod itaque exercitationem,
-          at ab sequi qui modi delectus quia corrupti alias distinctio nostrum.
-          Minima ex dolor modi inventore sapiente necessitatibus aliquam fuga et. Sed
-          numquam quibusdam at officia sapiente porro maxime corrupti perspiciatis
-          asperiores, exercitationem eius nostrum consequuntur iure aliquam itaque,
-          assumenda et! Quibusdam temporibus beatae doloremque voluptatum doloribus
-          soluta accusamus porro reprehenderit eos inventore facere, fugit, molestiae
-          ab officiis illo voluptates recusandae. Vel dolor nobis eius, ratione atque
-          soluta, aliquam fugit qui iste architecto perspiciatis. Nobis, voluptatem!
-          Cumque, eligendi unde aliquid minus quis sit debitis obcaecati error,
-          delectus quo eius exercitationem tempore. Delectus sapiente, provident
-          corporis dolorum quibusdam aut beatae repellendus est labore quisquam
-          praesentium repudiandae non vel laboriosam quo ab perferendis velit ipsa
-          deleniti modi! Ipsam, illo quod. Nesciunt commodi nihil corrupti cum non
-          fugiat praesentium doloremque architecto laborum aliquid. Quae, maxime
-          recusandae? Eveniet dolore molestiae dicta blanditiis est expedita eius
-          debitis cupiditate porro sed aspernatur quidem, repellat nihil quasi
-          praesentium quia eos, quibusdam provident. Incidunt tempore vel placeat
-          voluptate iure labore, repellendus beatae quia unde est aliquid dolor
-          molestias libero. Reiciendis similique exercitationem consequatur, nobis
-          placeat illo laudantium! Enim perferendis nulla soluta magni error,
-          provident repellat similique cupiditate ipsam, et tempore cumque quod! Qui,
-          iure suscipit tempora unde rerum autem saepe nisi vel cupiditate iusto.
-          Illum, corrupti? Fugiat quidem accusantium nulla. Aliquid inventore commodi
-          reprehenderit rerum reiciendis! Quidem alias repudiandae eaque eveniet
-          cumque nihil aliquam in expedita, impedit quas ipsum nesciunt ipsa ullam
-          consequuntur dignissimos numquam at nisi porro a, quaerat rem repellendus.
-          Voluptates perspiciatis, in pariatur impedit, nam facilis libero dolorem
-          dolores sunt inventore perferendis, aut sapiente modi nesciunt.
-        </Typography>
-      </Box>
+
     </Box>
-        </>
-    );
+  );
+}
+
+Header.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
 };
 
-export default Header;
--- src/Component/Share/LogIN_LogOut/LogIN_LogOut.js
+++ src/Component/Share/LogIN_LogOut/LogIN_LogOut.js
@@ -1,90 +1,91 @@
 import React, { useState } from 'react';
 import './LogIN_LogOut.css';
 import { BsFacebook, BsGoogle, BsLinkedin, BsLock, BsTwitter } from 'react-icons/bs';
-import {AiOutlineUser} from 'react-icons/ai'
-
+import { AiOutlineUser } from 'react-icons/ai'
+import {SiMinutemailer } from 'react-icons/si'
 
 
 const LogINLogOut = () => {
-    
+
     const [showSingUp, setShowSingUp] = useState(false);
 
     console.log(showSingUp);
 
     return (
-        <div>
-            <div className={`container ${showSingUp ? "sign-up-mode" : ' '}`}>
-                <div className="forms-container">
+        <div >
+            <div className={`containerr  ${showSingUp ? "sign-up-mode" : ' '}`}>
+                <div className="forms-containerr">
                     <div className="signin-signup">
                         <form action="#" className="sign-in-form">
                             <h2 className="title">Sign in</h2>
                             <div className="input-field">
                                 {/* <i className="fas fa-user"></i> */}
-                                <AiOutlineUser className='icon'/>
+                                <AiOutlineUser className='icon' />
                                 <input type="text" placeholder="Username" />
                             </div>
                             <div className="input-field">
                                 {/* <i className="fas fa-lock"></i> */}
-                                <BsLock className='icon'/>
+                                <BsLock className='icon' />
                                 <input type="password" placeholder="Password" />
                             </div>
                             <input type="submit" value="Login" className="btn solid" />
                             <p className="social-text">Or Sign in with social platforms</p>
                             <div className="social-media">
-                                <a href=" " className="social-icon">                                   
-                                    <BsFacebook/>
+                                <a href=" " className="social-icon">
+                                    <BsFacebook />
                                 </a>
                                 <a href=" " className="social-icon">
-                                    <BsTwitter/>      
+                                    <BsTwitter />
                                 </a>
                                 <a href=" " className="social-icon">
-                                    <BsGoogle/>
+                                    <BsGoogle />
                                 </a>
                                 <a href="  " className="social-icon">
-                                   
-                                    <BsLinkedin/>
+
+                                    <BsLinkedin />
                                 </a>
                             </div>
                         </form>
                         <form action=" " className="sign-up-form">
                             <h2 className="title">Sign up</h2>
                             <div className="input-field">
-                                <i className="fas fa-user"></i>
-                                <input type="text" placeholder="Username" required/>
+
+                                <AiOutlineUser className='icon' />
+                                <input type="text" placeholder="Username" required />
                             </div>
                             <div className="input-field">
-                                <i className="fas fa-envelope"></i>
-                                <input type="email" placeholder="Email"  required/>
+                                <SiMinutemailer className='icon' />
+                                <input type="email" placeholder="Email" required />
                             </div>
                             <div className="input-field">
-                                <i className="fas fa-lock"></i>
-                                <input type="password" placeholder="Password"  required/>
+                                <BsLock className='icon' />
+                                <input type="password" placeholder="Password" required />
                             </div>
                             <input type="submit" className="btn" value="Sign up" />
                             <p className="social-text">Or Sign up with social platforms</p>
                             <div className="social-media">
                                 <a href=" " className="social-icon">
-                                    
-                                    <BsFacebook/>
+
+                                    <BsFacebook />
                                 </a>
                                 <a href=" " className="social-icon">
-                                   
-                                    <BsLinkedin/>
+
+                                    <BsLinkedin />
                                 </a>
                                 <a href=" " className="social-icon">
-                                   
-                                    <BsGoogle/>
+
+                                    <BsGoogle />
                                 </a>
                                 <a href=" " className="social-icon">
-                                    
-                                    <BsTwitter/>
+
+                                    <BsTwitter />
                                 </a>
                             </div>
                         </form>
                     </div>
                 </div>
 
-                <div className="panels-container">
+                <div className="panels-containerr">
                     <div className="panel left-panel">
                         <div className="content">
                             <h3>New here ?</h3>
